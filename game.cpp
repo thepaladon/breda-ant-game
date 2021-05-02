f4d475cfc8aa9063177c97a91a89b02eecedd71c
thepaladon <angel@doncho.net>
2021-05-02 17:50:13 +0300

    project title
    added project title and inputs

--- game.cpp
+++ game.cpp
@@ -1,24 +1,39 @@
 #include "game.h"
 #include "surface.h"
-#include <cstdio> //printf
+#include <cstdio> 
 
 namespace Tmpl8
 {
+	int screenSelected = 1;
+	int mousecoordinateX, mousecoordinateY;
 
 	void Game::Init()
 	{
+
 	}
-	
 	
 	void Game::Shutdown()
 	{
+
 	}
 
 	void Game::Tick(float deltaTime)
 	{
-		printf("This is a test");
-		//this is yet another test
-		//yet yet another freaking test im so tired of git
+		if(screenSelected == 1)
+		{
+			screen->Print("Welcome to my game", 2, 2, 0xffffff);
+			
+		}
+		else if (screenSelected == 0) 
+		{
+			screen->Print("Start Screen", 2, 2, 0xffffff);
+		}
+		printf("mouse coordinates %i %i \n", mousex, mousey);
+		printf("mouse is pressed %i \n", mousePressed);
+		
+		mousecoordinateX += mousex;
+		mousecoordinateY += mousey;
+
 
 	}
 };
--- game.h
+++ game.h
@@ -6,15 +6,18 @@ class Surface;
 class Game
 {
 public:
+	int mousePressed;
+	int mousex, mousey;
 	void SetTarget( Surface* surface ) { screen = surface; }
 	void Init();
 	void Shutdown();
 	void Tick( float deltaTime );
 	void MouseUp( int button ) { /* implement if you want to detect mouse button presses */ }
-	void MouseDown( int button ) { /* implement if you want to detect mouse button presses */ }
-	void MouseMove( int x, int y ) { /* implement if you want to detect mouse movement */ }
+	void MouseDown(int button) { mousePressed = button; }
+	void MouseMove(int x, int y) { mousex = x, mousey = y; }
 	void KeyUp( int key ) { /* implement if you want to handle keys */ }
 	void KeyDown( int key ) { /* implement if you want to handle keys */ }
+
 private:
 	Surface* screen;
 };
