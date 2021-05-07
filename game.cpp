d6151b9a0a7772a9ed1b511d08ca4d963ce8d51b
thepaladon <angel@doncho.net>
2021-05-07 22:27:55 +0300

    New Pathfinding algorithm
    seems to do about the same job as the old one but it's

--- game.cpp
+++ game.cpp
@@ -27,9 +27,10 @@ namespace Tmpl8
 	int debugInt;
 	int numOfActiveAnts = 10;
 
+	//Ant Variables
 	float antSpeed = 1;
+	int distanceToObjToStop = 10;
 	bool confirmation;
-
 	Sprite ant(new Surface("assets/ant2scaleddown20.jpg"), 8);
 
 	class Ant
@@ -54,20 +55,30 @@ namespace Tmpl8
 			ant.Draw(gameScreen, x, y);
 			
 			//formula for calculating distance between two points
-			destinationToMarker = sqrt(pow(x - antDestinationX, 2) + pow(y - antDestinationY, 2));
+			distanceToMarker = sqrt(pow(x - antDestinationX, 2) + pow(y - antDestinationY, 2));
 
 			//draws blue outline for selected ants
 			if (antSelected == true) 				
 				gameScreen->Box(x, y, x + ANTSIZE, y + ANTSIZE, 0x29B1CA);
 
-			if (destinationToMarker <= 15)
+			if (distanceToMarker <= distanceToObjToStop)
 				antHeadingToDestination = false;
 			else if (antSelected && destinationPlaced && confirmation)
 				antHeadingToDestination = true;
 
 			BoxConfiguration();
+
 			if (antHeadingToDestination == true) {
+				//difference between the two pathfinding methods is almost none but the basic one is faster so I'll stick to that
+				
 				BasicPathfinding(antDestinationX, antDestinationY);
+
+				//BestPossiblePathCheck();
+				//AdvancedPathfinding();
+
+				//the best pathfinding method would be to use the same algorithm that calculates the lines 
+				//and then have the ant take the same path as the line
+				//I am not sure how to do that though
 			}
 		}
 
@@ -96,6 +107,31 @@ namespace Tmpl8
 			if (y1Max < y2Min || y1Min > y2Max) return false;
 
 			return true;
+		}
+
+		void BestPossiblePathCheck() {
+			float testCase[8];
+			float minValue = 1000;
+			int shotestToDest;
+
+			 testCase[0] = sqrt(pow((x + (distanceToObjToStop - 2)) - antDestinationX, 2) + pow((y + (distanceToObjToStop - 2)) - antDestinationY, 2));  //bottom right
+			 testCase[1] = sqrt(pow((x - (distanceToObjToStop - 2)) - antDestinationX, 2) + pow((y - (distanceToObjToStop - 2)) - antDestinationY, 2));	 //up left
+			 testCase[2] = sqrt(pow((x - (distanceToObjToStop - 2)) - antDestinationX, 2) + pow((y + (distanceToObjToStop - 2)) - antDestinationY, 2));  //bottom left
+			 testCase[3] = sqrt(pow((x + (distanceToObjToStop - 2)) - antDestinationX, 2) + pow((y - (distanceToObjToStop - 2)) - antDestinationY, 2));	 //up right
+			 testCase[4] = sqrt(pow(x - antDestinationX, 2) + pow((y - (distanceToObjToStop - 2)) - antDestinationY, 2));  //up
+			 testCase[5] = sqrt(pow(x - antDestinationX, 2) + pow((y + (distanceToObjToStop - 2)) - antDestinationY, 2));  //down
+			 testCase[6] = sqrt(pow((x - (distanceToObjToStop - 2)) - antDestinationX, 2) + pow(y - antDestinationY, 2));  //left
+			 testCase[7] = sqrt(pow((x + (distanceToObjToStop - 2)) - antDestinationX, 2) + pow(y - antDestinationY, 2));  //right
+
+			 for (int i = 0; i < 7; i++) {
+
+				 if (testCase[i] < minValue) {
+					 minValue = testCase[i];
+					 shotestToDest = i;
+				 }
+			 }
+
+			 bestDecision = shotestToDest;
 		}
 
 		void BasicPathfinding(int destX, int destY) {
@@ -149,6 +185,64 @@ namespace Tmpl8
 				x += antSpeed;
 			}
 			else if (destX - (ANTSIZE / 2) < x && destY - (ANTSIZE / 2) == y) {
+				//destination to the left
+				rotation = 2;
+				debugInt = 8;
+				x -= antSpeed;
+			}
+		} 
+		
+		void AdvancedPathfinding() {
+
+			if (bestDecision == 0) {
+				//destination in bottom right
+				rotation = 7;
+				debugInt = 4;
+				x += antSpeed;
+				y += antSpeed;
+			}
+			else if (bestDecision == 2) {
+				//destinaiton in bottom left
+				rotation = 6;
+				debugInt = 3;
+				x -= antSpeed;
+				y += antSpeed;
+			}
+			else if (bestDecision == 1) {
+				//destination in top left
+				rotation = 5;
+				debugInt = 2;
+				x -= antSpeed;
+				y -= antSpeed;
+
+			}
+			else if (bestDecision == 3) {
+				//destination in top right
+				rotation = 4;
+				debugInt = 1;
+				x += antSpeed;
+				y -= antSpeed;
+
+			}
+			else if (bestDecision == 5) {
+				//destinaiton below
+				rotation = 1;
+				debugInt = 5;
+				y += antSpeed;	
+			}
+			else if (bestDecision == 4) {
+				//destination above
+				rotation = 0;
+				debugInt = 6;
+				y -= antSpeed;
+			}
+			else if (bestDecision == 7) {
+				//destination to the right
+				rotation = 3;
+				debugInt = 7;
+				x += antSpeed;
+			}
+			else if (bestDecision == 6) {
 				//destination to the left
 				rotation = 2;
 				debugInt = 8;
@@ -214,7 +308,6 @@ namespace Tmpl8
 			}
 		}
 		
-
 		void BoxConfiguration() {
 			//Simple Visualisation saved in Notepad in Assets
 			//box dragged to top left
@@ -262,7 +355,8 @@ namespace Tmpl8
 		bool antSelected;
 		bool countedThisAntAlready;
 		bool antHeadingToDestination;
-		float destinationToMarker;
+		float distanceToMarker;
+		int bestDecision;
 	};
 
 	//[value] -> max ants on screen
@@ -289,7 +383,7 @@ namespace Tmpl8
 			for (int x = 0; x < 128; x++) //the x of the map is drawn
 				screen->Box(x * 10, y * 10, x * 10 + 10, y * 10 + 10, 0x3C2B0A);
 
-		screen->Print("Create more ants by pressing X", 20, 20, 0xffffff);
+		screen->Print("Create more ants by pressing CTRL", 20, 20, 0xffffff);
 		screen->Line(mousex - cursorSize, mousey - cursorSize, mousex + cursorSize, mousey + cursorSize, 0xff0000);
 		screen->Line(mousex - cursorSize, mousey + cursorSize, mousex + cursorSize, mousey - cursorSize, 0xff0000);
 
@@ -341,6 +435,7 @@ namespace Tmpl8
 			for (int n = 0; n < numOfActiveAnts; n++) {
 				if (myant[n].antSelected) {
 
+					screen->Line(myant[n].x + (ANTSIZE / 2), myant[n].y + (ANTSIZE / 2), myant[n].antDestinationX, myant[n].antDestinationY, 0xB00B69);
 					screen->Line(myant[n].antDestinationX - cursorSize, myant[n].antDestinationY - cursorSize, myant[n].antDestinationX + cursorSize, myant[n].antDestinationY + cursorSize, 0x29B1CA);
 					screen->Line(myant[n].antDestinationX - cursorSize, myant[n].antDestinationY + cursorSize, myant[n].antDestinationX + cursorSize, myant[n].antDestinationY - cursorSize, 0x29B1CA);
 				}
@@ -396,7 +491,7 @@ namespace Tmpl8
 		//printf("direction: %i \n", debugInt);
 		//printf("antDir: %i %i \n", antDestinationX, antDestinationY);
 		//printf("antCoords: %i %i \n", myant[1].x, myant[1].y);
-		//printf("ant 1 distance: %f \n", myant[1].destinationToMarker);
+		//printf("ant 1 distance: %f \n", myant[1].distanceToMarker);
 		//printf("mouse coordinates %i %i \n", mousex, mousey);
 		//printf("num of ants selected: %i \n", numSelected);
 		//printf("Box Style %i \n", boxStyle);
@@ -404,4 +499,5 @@ namespace Tmpl8
 		//printf("end X: %i  end Y: %i \n", boxEndX, boxEndY);
 
 	}
+
 };
